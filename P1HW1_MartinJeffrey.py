7229e32be3bb368c4e0a762c8750576f60ccd79b
martinj2139-maker <martinj2139@student.faytechcc.edu>
2026-03-01 16:39:10 -0500

    Add files via upload

---
+++ P1HW1_MartinJeffrey.py
@@ -0,0 +1,33 @@
+# Jeffrey Martin
+# 03/01/2026
+# P1HW1
+# Calculate Exponents
+#------Calculating Exponents------
+# Enter an integer as the base value: 7
+# Enter an integer as the exponent value: 3
+# 7 raised to the power of 3 is 343 !!
+
+#-------Addition and Subtraction------
+
+
+# Enter a starting integer: 10
+# Enter an integer to add: 6
+# Enter an integer to subtract: 4
+
+
+# 10 + 6 - 4 is 12 !!
+
+#-------Calculating Exponents------
+# Enter an integer as the base value: 8
+# Enter an integer as the exponent value: 4
+
+# 8 raised to the power of 4 is 4096 !!
+
+#-------Addition and Subtraction------
+
+# Enter a starting integer: 90
+# Enter an integer to add: 55
+# Enter an integer to subtract: 3
+
+
+# 90 + 55 - 3 is 142 !!
